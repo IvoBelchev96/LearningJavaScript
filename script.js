54e6b9d717832209aba2a1bcb4f0400ad2c1d1cc
IvoBelchev96 <166498568+IvoBelchev96@users.noreply.github.com>
2024-09-02 12:20:45 +0300

    Add files via upload
    The beginning of my JS journey starts here.

---
+++ script.js
@@ -0,0 +1,14 @@
+'use strict';
+
+function logger() {
+    console.log("My name is Jonas");
+}
+//calling/running/invoking a function.
+logger();
+logger();
+logger();
+
+function fruitProcessor(apples, oranges) {
+    console.log(apples, oranges);
+
+}
