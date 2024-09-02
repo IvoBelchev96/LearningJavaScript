1994cd6f82deb5abce58d8f03450dedc97acd035
IvoBelchev96 <ivo.belchev.a57@learn.telerikacademy.com>
2024-09-02 14:56:14 +0300

    test

--- script.js
+++ script.js
@@ -10,5 +10,9 @@ logger();
 
 function fruitProcessor(apples, oranges) {
     console.log(apples, oranges);
-
+    const juice = `Juice with ${apples} apples and ${oranges} oranges.`
+    return juice;
 }
+
+const appleJuice = fruitProcessor(5, 0);
+console.log(appleJuice)
